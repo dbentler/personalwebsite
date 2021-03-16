00869178b1cad742466e8a9dc16a9ab2ac6bd761
dbentler <47250652+dbentler@users.noreply.github.com>
2021-03-16 01:10:17 -0400

    Added Bootstrap, rewrote Navbar.
    Also rewrote the homepage.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
 
 app = Flask(__name__)
-nav = Nav(app) # Navbar baby
 
-nav.register_element('my_navbar', # Name of Navbar
-                     Navbar('thenav', # Navbar for html reference
-                     View('Home', 'home'), # View function :: View(<Text displayed>, <link that passthroughs to @app.route>)
-                     View('About Me', 'about')))
+#nav = Nav(app) # Navbar baby
+
+#nav.register_element('my_navbar', # Name of Navbar
+#                     Navbar('thenav', # Navbar for html reference
+#                     View('Home', 'home'), # View function :: View(<Text displayed>, <link that passthroughs to @app.route>)
+#                     View('About Me', 'about')))
 
 
 
@@ -21,6 +23,6 @@ def home(): # Function that is ran when <link>/<subpage> is true
 def about():
     return render_template("about.html")
 
-nav.init_app(app)
+#nav.init_app(app)
 if __name__ == "__main__":
     app.run(debug=True)

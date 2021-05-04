59b248c7e32b552a46e42a67fc4612bd173ab7aa
Darren Bentler <47250652+dbentler@users.noreply.github.com>
2021-05-03 20:39:07 -0400

    Finished homepage
    It's pretty now.

--- static/js/main.js
+++ static/js/main.js
@@ -11,3 +11,32 @@ function revealText(){
         button.innerHTML = "Reveal";
     }
 }
+
+function redirectGitHub(){
+    if(document.getElementById("github")){
+        location.href = "https://github.com/dbentler";
+    }
+}
+
+function redirectProjects(){
+    if(document.getElementById("projects")){
+        // location.href = "url/projects"
+        console.log("Projects page isn't set up just yet - sorry!")
+    }
+}
+
+function redirectLinkedIn(){
+    if(document.getElementById("linkedin")){
+        location.href = "https://www.linkedin.com/in/darren-bentler-255a36206/"
+    }
+}
+
+function redirectInstagram(){
+    if(document.getElementById("instagram")){
+        location.href = "https://www.instagram.com/dbentler/"
+    }
+}
+
+function downloadResume(){
+    //
+}

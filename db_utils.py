7ff00cb210bb525759657853854173a773648a06
dbentler <db3857@nyu.edu>
2021-05-08 19:39:37 -0400

    srv/markdown/test.md test

--- db_utils.py
+++ db_utils.py
@@ -11,7 +11,7 @@ def get_project_root() -> Path:
     return Path(__file__).parent.parent
 
 # UNCOMMENT FOR PRODUCTION
-ROOT = str(get_project_root())
+ROOT = str(get_project_root()) + "srv/"
 # UNCOMMENT FOR LOCAL DEVELOPMENT
 # ROOT = str(get_project_root()) + "\\"
 
